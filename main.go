12b325849849b3ac93aeb2d6939aa7dfe4c9f19f
cetinoka <okan.cetin@diconium.com>
2021-02-28 21:58:45 +0100

    Read Yaml File

--- main.go
+++ main.go
@@ -2,10 +2,18 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
 )
 
 func main() {
 	fmt.Println("Parsing configurations")
 
-	var fileName string
+	content, err := ioutil.ReadFile("./resources/deployment.yaml")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(string(content))
 }
